704613bb3911be4e44da0a9b7653c1e90a6f08fb
unknown <saem_ahmed@outlook.com>
2023-11-04 04:51:11 +0400

    group issue fix

---
+++ frontend/src/components/LoadingScreen.js
@@ -0,0 +1,11 @@
+import { useEffect, useRef, useState } from 'react';
+
+const LoadingScreen = () => {
+    return(
+        <div className='w-[100vh] h-[100vh] absolute'>
+
+        </div>
+    )
+}
+
+export default LoadingScreen;
