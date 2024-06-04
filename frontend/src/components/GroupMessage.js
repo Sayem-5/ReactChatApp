79315eb1ee9875c9ca8601394fdd7b127073ad44
Sayem-5 <140003659+Sayem-5@users.noreply.github.com>
2024-06-04 06:18:12 -0700

    revert groupMessage

--- frontend/src/components/GroupMessage.js
+++ frontend/src/components/GroupMessage.js
@@ -27,16 +27,16 @@ const GroupMessage = (prop) => {
         (
             <div className="message inline-flex pl-[25px] mb-3.5 font-open">
             <div className="bg-[#202c33] flex flex-col pl-[9px] pr-[7px] pb-[4px] pt-[5.5px] rounded-[9.67px] w-auto max-w-[45%]">  
-                    { message.sender?._id?.toString() === prevMessageSender?._id?.toString() ? (
-                        <div className="hidden"></div>
-                    ) : (
-                        <div className={`text-[12.8px] text-[#e9edef] flex flex-row items-center font-semibold tracking-wider`}>
-                            {/* <div className="bg-transparent border border-[#e9edef] mr-[2px] h-[6px] w-[6px] rounded-full"></div> */}
-                            <div style={senderColor ? { 'color' : senderColor.color } : {}}>
-                                {message.sender?.username}
-                            </div>
-                        </div>
-                    )}
+                    // { message.sender?._id?.toString() === prevMessageSender?._id?.toString() ? (
+                    //     <div className="hidden"></div>
+                    // ) : (
+                    //     <div className={`text-[12.8px] text-[#e9edef] flex flex-row items-center font-semibold tracking-wider`}>
+                    //         {/* <div className="bg-transparent border border-[#e9edef] mr-[2px] h-[6px] w-[6px] rounded-full"></div> */}
+                    //         <div style={senderColor ? { 'color' : senderColor.color } : {}}>
+                    //             {message.sender?.username}
+                    //         </div>
+                    //     </div>
+                    // )}
                     <div className="w-full pr-12 mt-[2px]">
                         <p className="text-[14.2px] text-[#e9edef] break-words">{message.content}</p>
                     </div>
